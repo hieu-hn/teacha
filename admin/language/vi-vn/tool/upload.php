badf9d446471dfc864f7f87662c8909e37b4dd84
Hieu Ha Ngoc <hieuhn@gmail.com>
2025-07-25 19:57:31 +0700

    update ngôn ngữ việt

---
+++ admin/language/vi-vn/tool/upload.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title']     = 'Tải lên';
+
+// Text
+$_['text_success']      = 'Thành công: Bạn đã sửa đổi tệp tải lên!';
+$_['text_list']         = 'Danh sách tải lên';
+
+// Column
+$_['column_name']       = 'Tên tải lên';
+$_['column_filename']   = 'Tên tệp';
+$_['column_date_added'] = 'Ngày thêm';
+$_['column_action']     = 'Thao tác';
+
+// Entry
+$_['entry_name']        = 'Tên tải lên';
+$_['entry_filename']    = 'Tên tệp';
+$_['entry_date_added']  = 'Ngày thêm';
+
+// Error
+$_['error_permission']  = 'Cảnh báo: Bạn không có quyền sửa đổi tệp tải lên!';
+$_['error_upload']      = 'Tải lên không hợp lệ!';
+$_['error_file']        = 'Không tìm thấy tệp tải lên!';
