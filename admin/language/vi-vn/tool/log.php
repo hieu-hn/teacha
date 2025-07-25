a844aa9ad3d612bd4961eb5c29d502e8e30b79c7
Hieu Ha Ngoc <hieuhn@gmail.com>
2025-07-25 19:57:31 +0700

    update ngôn ngữ việt

---
+++ admin/language/vi-vn/tool/log.php
@@ -0,0 +1,11 @@
+<?php
+// Heading
+$_['heading_title']    = 'Nhật ký lỗi';
+
+// Text
+$_['text_success']     = 'Thành công: Bạn đã xóa nhật ký lỗi thành công!';
+$_['text_list']        = 'Danh sách lỗi';
+
+// Error
+$_['error_warning']    = 'Cảnh báo: Tệp nhật ký lỗi của bạn %s là %s!';
+$_['error_permission'] = 'Cảnh báo: Bạn không có quyền xóa nhật ký lỗi!';
