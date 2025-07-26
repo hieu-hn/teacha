f9b490f4e017bb1912afeec7f94dd247c901aca8
cung-chay <78201217+cung-chay@users.noreply.github.com>
2025-07-26 15:55:32 +0000

    dịch sang tiếng việt

---
+++ catalog/language/vi-vn/extension/recurring/paypal.php
@@ -0,0 +1,8 @@
+<?php
+// Button
+$_['button_enable_recurring']				= 'Bật thanh toán định kỳ';
+$_['button_disable_recurring']				= 'Tắt thanh toán định kỳ';
+
+// Success
+$_['success_enable_recurring']				= 'Thành công: Thanh toán định kỳ đã được bật.';
+$_['success_disable_recurring']				= 'Thành công: Thanh toán định kỳ đã được tắt.';
---
+++ catalog/language/vi-vn/extension/recurring/squareup.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+$_['text_title']                = 'Square';
+$_['text_canceled']             = 'Thành công: Bạn đã hủy thành công khoản thanh toán này! Chúng tôi đã gửi email xác nhận cho bạn.';
+$_['text_confirm_cancel']       = 'Bạn có chắc chắn muốn hủy các khoản thanh toán định kỳ không?';
+$_['text_order_history_cancel'] = 'Bạn đã hủy hồ sơ thanh toán định kỳ. Thẻ của bạn sẽ không bị tính phí nữa.';
+
+// Button
+$_['button_cancel']             = 'Hủy thanh toán định kỳ';
+
+// Error
+$_['error_not_cancelled']       = 'Lỗi: %s';
+$_['error_not_found']           = 'Không thể hủy hồ sơ thanh toán định kỳ';
